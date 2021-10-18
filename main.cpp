b2e9e46bba6dd8b9d92cfdf06ef8585200d5d1cb
Jeyoung-Choi <000111000e@gmail.com>
2021-10-18 18:02:08 +0900

    27:49

--- main.cpp
+++ main.cpp
@@ -6,16 +6,18 @@
 #include <sstream>
 using namespace std;
 
-std::string readfile(const char* path)
+std::string readfile(const char *path)
 {
     ifstream ifs(path);
-    if (ifs.is_open()){
+    if (ifs.is_open())
+    {
         return std::string(istreambuf_iterator<char>(ifs), istreambuf_iterator<char>());
     }
     return nullptr;
 }
 
-int main(){
+int main()
+{
     auto vertexShaderSource = readfile("vert.glsl");
     auto fragmentShaderSource = readfile("frag.glsl");
 
@@ -26,37 +28,33 @@ int main(){
     // In this case we are using OpenGL 3.3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    //Tell GLFW we are using the CORE Profile
-    //So that means we only have the modern functions
+    // Tell GLFW we are using the CORE Profile
+    // So that means we only have the modern functions
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-
     GLfloat vertices[] = {
-        -0.5f, -0.5f * float(sqrt(3)) / 3 , 0.0f,
+        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
         0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-        0.0f, 0.5f * float(sqrt(3)) / 3, 0.0f
-    };
+        0.0f, 0.5f * float(sqrt(3)) / 3, 0.0f};
 
-
-
-    //Create GLFWwindow object of 800 by 800 pixels, naming it YoutubeOpenGL
-    GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
-    //Error check if the window fails to create
-    if (window == NULL){
+    // Create GLFWwindow object of 800 by 800 pixels, naming it YoutubeOpenGL
+    GLFWwindow *window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+    // Error check if the window fails to create
+    if (window == NULL)
+    {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
     // Introduce the window into the current context
     glfwMakeContextCurrent(window);
-    
+
     // Load GLAD so it configures OpenGL
     gladLoadGL();
 
-    //Specify the viewport of OpenGL in the window
-    // In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+    // Specify the viewport of OpenGL in the window
+    //  In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
     glViewport(0, 0, 800, 800);
-    
 
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource.c_str(), NULL);
@@ -77,27 +75,49 @@ int main(){
 
     // do not confuse front and back buffers....!!
 
-
-
-    GLuint VBO;
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    
 
-    //specify the color of the background
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
+
+    // specify the color of the background
     glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-    //clean the back buffer and assign the new color to it
+    // clean the back buffer and assign the new color to it
     glClear(GL_COLOR_BUFFER_BIT);
     // swap the back buffer with the front buffer
     glfwSwapBuffers(window);
-    
+
     // Main While loop (rendering loop)
-    while (!glfwWindowShouldClose(window)){
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+     
+            //27:49  https://www.youtube.com/watch?v=45MIykWJ-C4
+     
+
         // Take care of all GLFW events
         glfwPollEvents();
     }
-    //Destroy window
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    // Destroy window
     glfwDestroyWindow(window);
-    //Terminate GLFW
+    // Terminate GLFW
     glfwTerminate();
     return 0;
 }
